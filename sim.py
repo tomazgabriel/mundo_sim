5dba0671434196973ebff1f4db107a625fb31af9
tomazgabriel <gabriel.tomaz2@gmail.com>
2021-11-04 14:55:04 -0300

    ajuste na estrutura de empregos e educ

--- sim.py
+++ sim.py
@@ -2,14 +2,12 @@ import random
 import math
 
 pop_inicial = 50
-fator_crescimento = 1.0005
-mortalidade_infantil = 25
+
 idade_fertil_min = random.randint(14,18)
 idade_fertil_max = random.randint(35,40)
 
 lista_pessoas = []
 
-chance_disastre = 10
 
 comida = 0
 agricultura = 5
@@ -25,18 +23,48 @@ class Pessoas:
     def __init__(self, idade):
         self.sexo = random.randint(0,1)
         self.idade = idade
-        #chances da população ter mais inteligência aumentaram com a quantidade de educação
         self.educacao = educacao
-        self.inteligencia = random.randint(0,10)
+        def intel(self):
+            #chances da população ter mais inteligência aumentaram com a quantidade de educação
+            if educacao >= 0 and educacao <= 10:
+                int = [0,1,2,3,4,5,6,7,8,9,10]
+                peso =[10,9,8,7,6,5,4,3,2,1,1]
+                rand_intel = (random.choices(int, weights=peso, k=1))
+                self.inteligencia = rand_intel[0]
+            elif educacao >= 11 and educacao <= 20:
+                int = [0,1,2,3,4,5,6,7,8,9,10]
+                peso =[9,8,7,6,5,4,3,3,2,2,1]
+                rand_intel = (random.choices(int, weights=peso, k=1))
+                self.inteligencia = rand_intel[0]
+            elif educacao >= 21 and educacao <= 70:
+                int = [0,1,2,3,4,5,6,7,8,9,10]
+                peso =[7,6,5,5,4,4,3,3,3,2,2]
+                rand_intel = (random.choices(int, weights=peso, k=1))
+                self.inteligencia = rand_intel[0]
+            elif educacao >= 71 and educacao <= 80:
+                int = [0,1,2,3,4,5,6,7,8,9,10]
+                peso =[4,4,4,4,5,5,5,5,3,3,3]
+                rand_intel = (random.choices(int, weights=peso, k=1))
+                self.inteligencia = rand_intel[0]
+            elif educacao >= 81:
+                int = [0,1,2,3,4,5,6,7,8,9,10]
+                peso =[1,1,1,1,1,2,2,2,3,3,3]
+                rand_intel = (random.choices(int, weights=peso, k=1))
+                self.inteligencia = rand_intel[0]
+        def empregos(self):
+            #empregos 0 = desempregado, 1=campo, 2=educacao
+            for pessoa in lista_pessoas:
+                if pessoa.inteligencia >= 8:
+                    self.emprego = 2
+                else:
+                    self.emprego = 1
+        
         self.emprego = 0
-        #empregos 0 = desempregado, 1=campo, 2=educacao
-        for pessoa in lista_pessoas:
-            if pessoa.inteligencia >= 8:
-                self.emprego = 2
-            elif pessoa.inteligencia >= 1:
-                self.emprego = 1
-            else:
-                self.emprego = 0
+        self.inteligencia = 0
+
+        empregos(self)
+        intel(self)
+
 
 def agricultura_planeta(comida, agricultura, trabalhadores_campo):
     trabalhadores_campo = 0
@@ -66,7 +94,7 @@ def educacao_planeta(professores, educacao, pesquisa):
             if pessoa.emprego == 2:
                 professores += 1
     educacao += professores
-    pesquisa += math.ceil(educacao*2) #substituir o 2 por cientistas
+    pesquisa += int(educacao*2) #substituir o 2 por cientistas
     #immpedir divisão por 0
     if pesquisa <= 0:
         pesquisa += 1
@@ -92,7 +120,7 @@ def ano_corrente(comida, agricultura, idade_fertil_min, idade_fertil_max, trabal
 
 sim()
 
-while len(lista_pessoas) < 100 and len(lista_pessoas) > 1:
+while len(lista_pessoas) < 1000 and len(lista_pessoas) > 1:
     ano_corrente(comida, agricultura, idade_fertil_min, idade_fertil_max, trabalhadores_campo, professores, educacao, pesquisa)
     print("pop: ", len(lista_pessoas), "\nComida/Trabalhadores: {}".format(agricultura_planeta(comida, agricultura,trabalhadores_campo)))
     print("professores/educação/pesquisa {}".format(educacao_planeta(professores,educacao,pesquisa)))
