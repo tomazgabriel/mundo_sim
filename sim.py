211099884d1c840adb7a48ad5a5537d037b456cb
tomazgabriel <gabriel.tomaz2@gmail.com>
2021-11-03 23:37:18 -0300

    adicionado educacao

--- sim.py
+++ sim.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 pop_inicial = 50
 fator_crescimento = 1.0005
@@ -13,20 +14,37 @@ chance_disastre = 10
 comida = 0
 agricultura = 5
 
+educacao = 0
+pesquisa = 0
+
 #trabalhos
 trabalhadores_campo = 0
+professores = 0
 
 class Pessoas:
     def __init__(self, idade):
         self.sexo = random.randint(0,1)
         self.idade = idade
+        #chances da população ter mais inteligência aumentaram com a quantidade de educação
+        self.educacao = educacao
+        self.inteligencia = random.randint(0,10)
+        self.emprego = 0
+        #empregos 0 = desempregado, 1=campo, 2=educacao
+        for pessoa in lista_pessoas:
+            if pessoa.inteligencia >= 8:
+                self.emprego = 2
+            elif pessoa.inteligencia >= 1:
+                self.emprego = 1
+            else:
+                self.emprego = 0
 
 def agricultura_planeta(comida, agricultura, trabalhadores_campo):
     trabalhadores_campo = 0
     for pessoa in lista_pessoas:
         if pessoa.idade > 18:
-            trabalhadores_campo += 1
-    comida += trabalhadores_campo*agricultura
+            if pessoa.emprego == 1:
+                trabalhadores_campo += 1
+    comida += trabalhadores_campo*agricultura #ajustar agricultura para nível tecnológico posteriormente
     if comida < len(lista_pessoas):
         del lista_pessoas[0:int(len(lista_pessoas)-comida)]
         comida = 0
@@ -39,16 +57,31 @@ def reproducao(idade_fertil_min, idade_fertil_max):
         if pessoa.sexo == 1:
             if pessoa.idade > idade_fertil_min:
                 if pessoa.idade < idade_fertil_max:
-                    if random.randint(1,5) == 1:
+                    if random.randint(1,5) == 1: # 1 em cada 5 engravidará
                         lista_pessoas.append(Pessoas(0))
+
+def educacao_planeta(professores, educacao, pesquisa):
+    for pessoa in lista_pessoas:
+        if pessoa.idade > 25:
+            if pessoa.emprego == 2:
+                professores += 1
+    educacao += professores
+    pesquisa += math.ceil(educacao*2) #substituir o 2 por cientistas
+    #immpedir divisão por 0
+    if pesquisa <= 0:
+        pesquisa += 1
+    if educacao <= 0:
+        educacao += 1
+    return professores, educacao, pesquisa
 
 def sim():
     for x in range(pop_inicial):
         lista_pessoas.append(Pessoas(random.randint(18,50)))
 
-def ano_corrente(comida, agricultura, idade_fertil_min, idade_fertil_max, trabalhadores_campo):
+def ano_corrente(comida, agricultura, idade_fertil_min, idade_fertil_max, trabalhadores_campo, professores, educacao, pesquisa):
     agricultura_planeta(comida, agricultura, trabalhadores_campo)
     reproducao(idade_fertil_min,idade_fertil_max)
+    educacao_planeta(professores, educacao, pesquisa)
 
     for pessoa in lista_pessoas:
         if pessoa.idade > 80:
@@ -59,9 +92,10 @@ def ano_corrente(comida, agricultura, idade_fertil_min, idade_fertil_max, trabal
 
 sim()
 
-while len(lista_pessoas) < 10000 and len(lista_pessoas) > 1:
-    ano_corrente(comida, agricultura, idade_fertil_min, idade_fertil_max, trabalhadores_campo)
+while len(lista_pessoas) < 100 and len(lista_pessoas) > 1:
+    ano_corrente(comida, agricultura, idade_fertil_min, idade_fertil_max, trabalhadores_campo, professores, educacao, pesquisa)
     print("pop: ", len(lista_pessoas), "\nComida/Trabalhadores: {}".format(agricultura_planeta(comida, agricultura,trabalhadores_campo)))
+    print("professores/educação/pesquisa {}".format(educacao_planeta(professores,educacao,pesquisa)))
 
-#teste
+
     
